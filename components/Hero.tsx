b2e7aafdb11863db28b467678bc61ffc2a32547e
Daniel Amenyenu <ameneynudaniel45@gmail.com>
2023-12-14 13:41:28 +0000

    .env removed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Image from "next/image";
-
 import { CustomButton } from "@components";
 
 const Hero = () => {
--- utils/index.ts
+++ utils/index.ts
@@ -48,7 +48,8 @@ export async function fetchCars(filters: FilterProps) {
 
   // Set the required headers for the API request
   const headers: HeadersInit = {
-    "X-RapidAPI-Key": process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
+    "X-RapidAPI-Key":
+      "619e99bce1msh0f8497b704383d2p148a12jsn897607ec2749" || "",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
@@ -70,10 +71,7 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
   const { make, model, year } = car;
 
-  url.searchParams.append(
-    "customer",
-    process.env.NEXT_PUBLIC_IMAGIN_API_KEY || ""
-  );
+  url.searchParams.append("customer", "hrjavascript-mastery" || "");
   url.searchParams.append("make", make);
   url.searchParams.append("modelFamily", model.split(" ")[0]);
   url.searchParams.append("zoomType", "fullscreen");
